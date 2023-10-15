1c7dbda0f94a152da21ae4922f103d36d6956e0b
Мирослав Косюк <87585769+Baldezxniy@users.noreply.github.com>
2023-10-15 23:24:10 +0300

    content only via telegram

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import './App.css'
 import { Header } from './components/Header';
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+
+  if (Boolean(tg.initDataUnsafe)) {
+    return <div className="use_telegram">Используйте телеграмм для входа!</div>
+  }
+
   return (
     <>
       <Header />
