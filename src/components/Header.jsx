de7cd255bd8e436b19c507d97acb4d730cbdcc4f
Мирослав Косюк <87585769+Baldezxniy@users.noreply.github.com>
2023-10-16 01:31:26 +0300

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,7 @@ export const Header = (props) => {
     return (
         <header className="header">
             <Button onClick={onClose}>Закрыть</Button>
+            
             <span className="username">{user?.username}</span>
         </header>
     );
